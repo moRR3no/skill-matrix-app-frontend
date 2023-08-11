fb541fa06fd5656f993598c9768ddf739600abd9
morreno <konradpstrongowski@gmail.com>
2023-08-11 10:30:27 +0200

    Initial commit

--- skill-matrix-app/src/app/app-routing.module.ts
+++ skill-matrix-app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './features/employee/employee.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { EmployeeDetailComponent } from './features/employee-detail/employee-detail.component';
+import { EmployeeModule } from './features/employee/employee.module';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -13,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), EmployeeModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
